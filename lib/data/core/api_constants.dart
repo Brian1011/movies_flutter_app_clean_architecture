cffc1bdc10b076bcf0a1f5475939a005bfe187ad
Brian Mutinda <mutindabrian26@gmail.com>
2023-04-26 12:01:31 +0300

    create keys file

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,9 @@
+import 'package:movieapp/keys.dart';
+
+class ApiConstants {
+  ApiConstants._();
+
+  static const BASE_URL = "https://api.themoviedb.org/3/";
+  // pick your own api key from https://www.themoviedb.org/settings/api
+  static const API_KEY = ApiKeys.movieKey;
+}
---
+++ lib/keys.example.dart
@@ -0,0 +1,3 @@
+class ApiKeys {
+  static const movieKey = "12345678901234567890123456789012";
+}
